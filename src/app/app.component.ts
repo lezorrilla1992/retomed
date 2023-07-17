c1ae527097a44cb932b4ad471e67b7b2825e9208
Luis Ernesto Zorrilla PIla <lezorrilla1992@gmail.com>
2023-07-17 12:45:08 -0400

    button toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import{BreakpointObserver} from '@angular/cdk/layout'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'retomed';
+  @ViewChild(MatSidenav)
+  sidenav!:MatSidenav;
+
+  constructor(private observer: BreakpointObserver){
+     
+  }
+  ngAfterViewInit(){
+    this.observer.observe(['(max-width: 800px)']).subscribe((resp:any) =>{
+      if (resp.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      }
+      else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    })
+  }
 }
